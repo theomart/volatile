35cbed335582a8a6dbd4a3c83204ccd28323e8a4
Theo <theo.martin21@gmail.com>
2022-03-11 14:04:36 +0100

    Refactoring
    - Replaced multitasking by aiohttp for fetching yahoo finance data
    - Replaced home made progress bar by tqdm
    - Renamed more explicitly variables and functions
    - Print some reports on the tickers we could and couldnt fetch
    - Split functions fetching stock info and stock quotes
    - Fixed some Optional typing

--- download.py
+++ download.py
@@ -1,17 +1,132 @@
 #!/usr/bin/env python
-import os.path
+import logging
+import aiohttp
+import asyncio
+from tqdm.asyncio import tqdm_asyncio
+from tqdm.contrib.logging import logging_redirect_tqdm
 import pandas as pd
-import requests
-import multitasking
 import json
-import csv
 import numpy as np
+import time
 import datetime as dt
-from typing import Union
+from typing import Optional, Union
 
-from tools import ProgressBar
+logger = logging.getLogger(__name__)
 
-def download(tickers: list, start: Union[str, int] = None, end: Union[str, int] = None, interval: str = "1d") -> dict:
+
+async def _download_single_ticker_info(session: aiohttp.ClientSession, ticker: str):
+    """
+    Download historical data for a single ticker with multithreading. Plus, it scrapes missing stock information.
+
+    Parameters
+    ----------
+    ticker: str
+        Ticker for which to download historical information.
+    interval: str
+        Frequency between data.
+    start: str
+        Start download data from this date.
+    end: str
+        End download data at this date.
+    """
+
+    
+    try:
+        async with session.get("https://finance.yahoo.com/quote/" + ticker) as response:
+            html = await response.text()
+        json_str = html.split("root.App.main =")[1].split("(this)")[0].split(";\n}")[0].strip()
+        info = json.loads(json_str)["context"]["dispatcher"]["stores"]["QuoteSummaryStore"]["summaryProfile"]
+        assert (len(info["sector"]) > 0) and (len(info["industry"]) > 0)
+        return {"SYMBOL": ticker, "SECTOR": info["sector"], "INDUSTRY": info["industry"]}
+    except Exception as e:
+        return {"SYMBOL": ticker, "error": e}
+
+        
+
+
+
+async def _download_single_ticker_chart_data(
+    session: aiohttp.ClientSession, ticker: str, start: int, end: int, interval: str = "1d"
+) -> dict:
+    """
+    Download historical data for a single ticker.
+
+    Parameters
+    ----------
+    ticker: str
+        Ticker for which to download historical information.
+    start: int
+        Start download data from this timestamp date.
+    end: int
+        End download data at this timestamp date.
+    interval: str
+        Frequency between data.
+
+    Returns
+    -------
+    data: dict
+        Scraped dictionary of information.
+    """
+    url = f"https://query1.finance.yahoo.com/v8/finance/chart/{ticker}"
+    params = dict(period1=start, period2=end, interval=interval.lower(), includePrePost="false")
+
+    async with session.get(url, params=params) as response:
+        data_text = await response.text()
+
+        if "Will be right back" in data_text:
+            raise RuntimeError("*** YAHOO! FINANCE is currently down! ***\n")
+        else:
+            response_json = await response.json()
+    try:
+        raw_quotes = response_json["chart"]["result"][0]
+        currency = raw_quotes["meta"]["currency"]
+        return {"ticker": ticker, "quotes": _parse_quotes(raw_quotes), "currency": currency}
+    except Exception as e:
+        if "error" in response_json.get("chart", {}):
+            e = response_json["chart"]["error"]
+        return {"ticker": ticker, "error": e}
+
+async def async_download_tickers_info(
+    tickers: list, tickers_missing_info: list, start: int, end: int, interval: str,
+):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+    }
+    async with aiohttp.ClientSession(headers=headers) as session:
+        print("\nDownloading stock industry and sector")
+        with logging_redirect_tqdm():
+            tickers_info = await tqdm_asyncio.gather(
+                *[_download_single_ticker_info(session, ticker) for ticker in tickers_missing_info]
+            )
+
+    no_error_tickers_info = [ticker_info for ticker_info in tickers_info if "error" not in ticker_info]
+    errored_tickers_info = [ticker_info["SYMBOL"] for ticker_info in tickers_info if "error" in ticker_info]
+    print(f"Out of {len(tickers_missing_info)} tickers missing info, we could get {len(no_error_tickers_info)}")
+    print(f"Couldn't get info: {', '.join(errored_tickers_info)}")
+
+
+    async with aiohttp.ClientSession(headers=headers) as session:
+        print("\nDownloading stock quotes")
+        with logging_redirect_tqdm():
+            all_tickers_chart_data = await tqdm_asyncio.gather(
+                *[_download_single_ticker_chart_data(session, ticker, start, end, interval) for ticker in tickers]
+            )
+    quotes = {ticker_dict["ticker"]: ticker_dict["quotes"] for ticker_dict in all_tickers_chart_data if "error" not in ticker_dict}
+    errored_tickers_quotes = [ticker_dict["ticker"] for ticker_dict in all_tickers_chart_data if "error" in ticker_dict]
+    print(f"Out of {len(tickers)} tickers, we could get quotes for {len(quotes)}")
+    print(f"Couldn't get quotes for: {', '.join(errored_tickers_quotes)}")
+    quotes_df = pd.concat(quotes.values(), keys=quotes.keys(), axis=1, sort=True)
+
+    stock_info_df = pd.DataFrame(no_error_tickers_info, columns=["SYMBOL", "CURRENCY", "SECTOR", "INDUSTRY"])
+    currencies = {ticker_dict["ticker"]: ticker_dict["currency"] for ticker_dict in all_tickers_chart_data if "error" not in ticker_dict}
+    stock_info_df["CURRENCY"] = stock_info_df["SYMBOL"].apply(currencies.get)
+
+    return quotes_df, stock_info_df
+
+
+def download_tickers_info(
+    tickers: list, start: Optional[str] = None, end: Optional[str] = None, interval: str = "1d"
+) -> dict:
     """
     Download historical data for tickers in the list.
 
@@ -36,145 +151,75 @@ def download(tickers: list, start: Union[str, int] = None, end: Union[str, int] 
         - sectors: dictionary of stock sector for each ticker;
         - industries: dictionary of stock industry for each ticker.
     """
-    tickers = tickers if isinstance(tickers, (list, set, tuple)) else tickers.replace(',', ' ').split()
+    tickers = tickers if isinstance(tickers, (list, set, tuple)) else tickers.replace(",", " ").split()
     tickers = list(set([ticker.upper() for ticker in tickers]))
 
-    data = {}
-    si_columns = ["SYMBOL", "CURRENCY", "SECTOR", "INDUSTRY"]
-    si_filename = "stock_info.csv"
-    if not os.path.exists(si_filename):
-        # create a .csv to store stock information
-        with open(si_filename, 'w') as file:
-            wr = csv.writer(file)
-            wr.writerow(si_columns)
-    # load stock information file
-    si = pd.read_csv(si_filename)
-    missing_tickers = [ticker for ticker in tickers if ticker not in si['SYMBOL'].values]
-    missing_si, na_si = {}, {}
-    currencies = {}
+    logger.info(f"Downloading data for {len(tickers)} tickers")
 
-    if end is None:
-        end = int(dt.datetime.timestamp(dt.datetime.today()))
-    elif type(end) is str:
-        end = int(dt.datetime.timestamp(dt.datetime.strptime(end, '%Y-%m-%d')))
-    if start is None:
-        start = int(dt.datetime.timestamp(dt.datetime.today() - dt.timedelta(365)))
-    elif type(start) is str:
-        start = int(dt.datetime.timestamp(dt.datetime.strptime(start, '%Y-%m-%d')))
+    stock_info_columns = ["SYMBOL", "CURRENCY", "SECTOR", "INDUSTRY"]
+    stock_info_filename = "stock_info.csv"
 
-    @multitasking.task
-    def _download_one_threaded(ticker: str, start: str, end: str, interval: str = "1d"):
-        """
-        Download historical data for a single ticker with multithreading. Plus, it scrapes missing stock information.
+    try:
+        cached_stock_info_df = pd.read_csv(stock_info_filename)
+        logger.info(f"Reading stock info found in file '{stock_info_filename}'")
+    except FileNotFoundError:
+        cached_stock_info_df = pd.DataFrame(columns=stock_info_columns)
 
-        Parameters
-        ----------
-        ticker: str
-            Ticker for which to download historical information.
-        interval: str
-            Frequency between data.
-        start: str
-            Start download data from this date.
-        end: str
-            End download data at this date.
-        """
-        data_one = _download_one(ticker, start, end, interval)
+    tickers_already_fetched_info = cached_stock_info_df["SYMBOL"].values
 
-        try:
-            data_one = data_one["chart"]["result"][0]
-            data[ticker] = _parse_quotes(data_one)
+    tickers_misssing_info = [ticker for ticker in tickers if ticker not in tickers_already_fetched_info]
 
-            if ticker in missing_tickers:
-                currencies[ticker] = data_one['meta']['currency']
-                try:
-                    html = requests.get(url='https://finance.yahoo.com/quote/' + ticker).text
-                    json_str = html.split('root.App.main =')[1].split('(this)')[0].split(';\n}')[0].strip()
-                    info = json.loads(json_str)['context']['dispatcher']['stores']['QuoteSummaryStore']['summaryProfile']
-                    assert (len(info['sector']) > 0) and (len(info['industry']) > 0)
-                    missing_si[ticker] = dict(sector=info["sector"], industry=info["industry"])
-                except:
-                    pass
-        except:
-            pass
-        progress.animate()
+    end = int(time.time()) if end is None else int(dt.datetime.strptime(end, "%Y-%m-%d").timestamp())
+    start = (
+        int((dt.datetime.today() - dt.timedelta(365)).timestamp())
+        if start is None
+        else int(dt.datetime.strptime(start, "%Y-%m-%d").timestamp())
+    )
 
-    num_threads = min([len(tickers), multitasking.cpu_count() * 2])
-    multitasking.set_max_threads(num_threads)
+    stocks_quotes_df, newly_fetched_stock_info_df = asyncio.run(
+        async_download_tickers_info(tickers, tickers_misssing_info, start, end, interval)
+    )
 
-    progress = ProgressBar(len(tickers), 'completed')
+    full_stock_info_df = pd.concat([cached_stock_info_df, newly_fetched_stock_info_df])
+    full_stock_info_df.to_csv(stock_info_filename, index=False)
 
-    for ticker in tickers:
-        _download_one_threaded(ticker, start, end, interval)
-    multitasking.wait_for_tasks()
-
-    progress.completed()
-
-    if len(data) == 0:
+    if stocks_quotes_df.shape[0] == 0:
         raise Exception("No symbol with full information is available.")
 
-    data = pd.concat(data.values(), keys=data.keys(), axis=1, sort=True)
-    data.drop(columns=data.columns[data.isnull().sum(0) > 0.33 * data.shape[0]], inplace=True)
-    data = data.fillna(method='bfill').fillna(method='ffill').drop_duplicates()
+    stocks_quotes_df = stocks_quotes_df.drop(
+        columns=stocks_quotes_df.columns[stocks_quotes_df.isnull().sum(0) > 0.33 * stocks_quotes_df.shape[0]]
+    )
+    stocks_quotes_df = stocks_quotes_df.fillna(method="bfill").fillna(method="ffill").drop_duplicates()
 
-    info = zip(list(missing_si.keys()), [currencies[ticker] for ticker in missing_si.keys()],
-                                        [v['sector'] for v in missing_si.values()],
-                                        [v['industry'] for v in missing_si.values()])
-    with open(si_filename, 'a+', newline='') as file:
-        wr = csv.writer(file)
-        for row in info:
-            wr.writerow(row)
-    si = pd.read_csv('stock_info.csv').set_index("SYMBOL").to_dict(orient='index')
-
-    missing_tickers = [ticker for ticker in tickers if ticker not in data.columns.get_level_values(0)[::2].tolist()]
-    tickers = data.columns.get_level_values(0)[::2].tolist()
-    if len(missing_tickers) > 0:
-        print('\nRemoving {} from list of symbols because we could not collect full information.'.format(missing_tickers))
+    tickers_misssing_info = [
+        ticker for ticker in tickers if ticker not in stocks_quotes_df.columns.get_level_values(0)[::2].tolist()
+    ]
+    tickers = stocks_quotes_df.columns.get_level_values(0)[::2].tolist()
+    if len(tickers_misssing_info) > 0:
+        print(
+            f"\nRemoving {tickers_misssing_info} from list of symbols because we could not collect full information."
+        )
 
     # download exchange rates and convert to most common currency
-    currencies = [si[ticker]['CURRENCY'] if ticker in si else currencies[ticker] for ticker in tickers]
-    ucurrencies, counts = np.unique(currencies, return_counts=True)
-    default_currency = ucurrencies[np.argmax(counts)]
-    xrates = get_exchange_rates(currencies, default_currency, data.index, start, end, interval)
+    currencies = full_stock_info_df["CURRENCY"].to_list()
+    default_currency = full_stock_info_df["CURRENCY"].mode()[0]
+    xrates = get_exchange_rates(currencies, default_currency, stocks_quotes_df.index, start, end, interval)
 
-    return dict(tickers=tickers,
-                dates=pd.to_datetime(data.index),
-                price=data.iloc[:, data.columns.get_level_values(1) == 'Adj Close'].to_numpy().T,
-                volume=data.iloc[:, data.columns.get_level_values(1) == 'Volume'].to_numpy().T,
-                currencies=currencies,
-                exchange_rates=xrates,
-                default_currency=default_currency,
-                sectors={ticker: si[ticker]['SECTOR'] if ticker in si else "NA_" + ticker for ticker in tickers},
-                industries={ticker: si[ticker]['INDUSTRY'] if ticker in si else "NA_" + ticker for ticker in tickers})
-
-def _download_one(ticker: str, start: int, end: int, interval: str = "1d") -> dict:
-    """
-    Download historical data for a single ticker.
-
-    Parameters
-    ----------
-    ticker: str
-        Ticker for which to download historical information.
-    start: int
-        Start download data from this timestamp date.
-    end: int
-        End download data at this timestamp date.
-    interval: str
-        Frequency between data.
-
-    Returns
-    -------
-    data: dict
-        Scraped dictionary of information.
-    """
-    base_url = 'https://query1.finance.yahoo.com'
-    params = dict(period1=start, period2=end, interval=interval.lower(), includePrePost=False)
-    url = "{}/v8/finance/chart/{}".format(base_url, ticker)
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-    data = requests.get(url=url, params=params, headers=headers)
-    if "Will be right back" in data.text:
-        raise RuntimeError("*** YAHOO! FINANCE is currently down! ***\n")
-    data = data.json()
-    return data
+    return dict(
+        tickers=tickers,
+        dates=pd.to_datetime(stocks_quotes_df.index),
+        price=stocks_quotes_df.iloc[:, stocks_quotes_df.columns.get_level_values(1) == "Adj Close"].to_numpy().T,
+        volume=stocks_quotes_df.iloc[:, stocks_quotes_df.columns.get_level_values(1) == "Volume"].to_numpy().T,
+        currencies=currencies,
+        exchange_rates=xrates,
+        default_currency=default_currency,
+        sectors={
+            stock_info_row["SYMBOL"]: stock_info_row["SECTOR"] for _, stock_info_row in full_stock_info_df.iterrows()
+        },
+        industries={
+            stock_info_row["SYMBOL"]: stock_info_row["INDUSTRY"] for _, stock_info_row in full_stock_info_df.iterrows()
+        },
+    )
 
 
 def _parse_quotes(data: dict, parse_volume: bool = True) -> pd.DataFrame:
@@ -211,12 +256,19 @@ def _parse_quotes(data: dict, parse_volume: bool = True) -> pd.DataFrame:
     quotes = pd.DataFrame(quotes)
     quotes.index = pd.to_datetime(timestamps, unit="s").date
     quotes.sort_index(inplace=True)
-    quotes = quotes.loc[~quotes.index.duplicated(keep='first')]
+    quotes = quotes.loc[~quotes.index.duplicated(keep="first")]
 
     return quotes
 
-def get_exchange_rates(from_currencies: list, to_currency: str, dates: pd.Index, start: Union[str, int] = None,
-                       end: Union[str, int] = None, interval: str = "1d") -> dict:
+
+def get_exchange_rates(
+    from_currencies: list,
+    to_currency: str,
+    dates: pd.Index,
+    start: Union[str, int] = None,
+    end: Union[str, int] = None,
+    interval: str = "1d",
+) -> dict:
     """
     It finds the most common currency and set it as default one. For any other currency, it downloads exchange rate
     closing prices to the default currency and return them as data frame.
@@ -244,24 +296,24 @@ def get_exchange_rates(from_currencies: list, to_currency: str, dates: pd.Index,
     if end is None:
         end = int(dt.datetime.timestamp(dt.datetime.today()))
     elif type(end) is str:
-        end = int(dt.datetime.timestamp(dt.datetime.strptime(end, '%Y-%m-%d')))
+        end = int(dt.datetime.timestamp(dt.datetime.strptime(end, "%Y-%m-%d")))
     if start is None:
         start = int(dt.datetime.timestamp(dt.datetime.today() - dt.timedelta(365)))
     elif type(start) is str:
-        start = int(dt.datetime.timestamp(dt.datetime.strptime(start, '%Y-%m-%d')))
+        start = int(dt.datetime.timestamp(dt.datetime.strptime(start, "%Y-%m-%d")))
 
     ucurrencies, counts = np.unique(from_currencies, return_counts=True)
     tmp = {}
     if to_currency not in ucurrencies or len(ucurrencies) > 1:
         for curr in ucurrencies:
             if curr != to_currency:
-                tmp[curr] = _download_one(curr + to_currency + "=x", start, end, interval)
+                tmp[curr] = _download_single_ticker_chart_data(curr + to_currency + "=x", start, end, interval)
                 tmp[curr] = _parse_quotes(tmp[curr]["chart"]["result"][0], parse_volume=False)["Adj Close"]
         tmp = pd.concat(tmp.values(), keys=tmp.keys(), axis=1, sort=True)
         xrates = pd.DataFrame(index=dates, columns=tmp.columns)
         xrates.loc[xrates.index.isin(tmp.index)] = tmp
-        xrates = xrates.fillna(method='bfill').fillna(method='ffill')
-        xrates.to_dict(orient='list')
+        xrates = xrates.fillna(method="bfill").fillna(method="ffill")
+        xrates.to_dict(orient="list")
     else:
         xrates = tmp
     return xrates
